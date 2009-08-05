30331be66e090b1a53c7c9b4d4b86c1a61b6c5c4
danhut <danhut@d5303d96-4916-11de-8855-c187e5f49513>
2009-08-05 04:49:37 +0000

    hack để enable hiển thị blocks trong html resource page

--- trunk/SmartLMS/mod/resource/type/html/resource.class.php
+++ trunk/SmartLMS/mod/resource/type/html/resource.class.php
@@ -1,5 +1,10 @@
 <?php // $Id: resource.class.php,v 1.40.2.3 2008/07/01 22:25:26 skodak Exp $
 
+require_once($CFG->libdir.'/blocklib.php');
+require_once($CFG->libdir.'/pagelib.php');
+require_once($CFG->dirroot.'/mod/resource/pagelib.php');
+
+
 class resource_html extends resource_base {
 
 
@@ -59,17 +64,25 @@ function display() {
     // fix for MDL-15387, thanks to John Beedell
     add_to_log($course->id, "resource", "view", "view.php?id={$cm->id}", $resource->id, $cm->id);
 
+    /*danhut addded to enable blocks in resource page*/
+    $PAGE       = page_create_instance($resource->id);
+    $pageblocks = blocks_setup($PAGE);
+    $blocks_preferred_width = bounded_number(180, blocks_preferred_width($pageblocks[BLOCK_POS_LEFT]), 210);
+    /*end of danhut addded*/
+    
+   /*danhut commented out: khôgn hiển thị toàn bộ block ở course page vào module page nữa*/
     /// Are we displaying the course blocks?
-    if ($this->resource->options == 'showblocks') {
+//    if ($this->resource->options == 'showblocks') {
+//
+//        parent::display_course_blocks_start();
+//
+//        echo format_text($this->resource->alltext, FORMAT_HTML, $formatoptions, $this->course->id);
+//
+//        parent::display_course_blocks_end();
+//
+//    } else {
 
-        parent::display_course_blocks_start();
-
-        echo format_text($this->resource->alltext, FORMAT_HTML, $formatoptions, $this->course->id);
-
-        parent::display_course_blocks_end();
-
-    } else {
-
+    /*end of danhut's commentation*/
         /// Set up generic stuff first, including checking for access
         parent::display();
 
@@ -82,6 +95,8 @@ function display() {
                 print_header();
                 print_simple_box(format_text($resource->alltext, FORMAT_HTML, $formatoptions, $course->id),
                         "center clearfix", "", "", "20");
+                
+                
                 print_footer($course);
             } else {                           /// Make a page and a pop-up window
                 $navigation = build_navigation($this->navlinks, $cm);
@@ -107,7 +122,10 @@ function display() {
                 echo '<br />';
                 print_string('popupresourcelink', 'resource', $link);
                 echo '</div>';
-
+                /*danhut added*/
+                print_container_end();
+    			echo '</td></tr></table>';
+    			/*end of danhut added*/
                 print_footer($course);
             }
         } else {    /// not a popup at all
@@ -117,15 +135,32 @@ function display() {
                     "", "", true, update_module_button($cm->id, $course->id, $this->strresource),
                     navmenu($course, $cm));
 
+            /*danhut added*/
+		    echo '<table id="layout-table"><tr>';
+			if(!empty($CFG->showblocksonmodpages) && (blocks_have_content($pageblocks, BLOCK_POS_LEFT) || $PAGE->user_is_editing())) {
+		        echo '<td style="width: '.$blocks_preferred_width.'px;" id="left-column">';
+		        print_container_start();
+		        blocks_print_group($PAGE, $pageblocks, BLOCK_POS_LEFT);
+		        print_container_end();
+		        echo '</td>';
+		    }
+            echo '<td id="middle-column">';
+    		print_container_start();
+    		/*end of danhut added*/
             print_simple_box(format_text($resource->alltext, FORMAT_HTML, $formatoptions, $course->id), "center clearfix", "", "", "20");
 
             $strlastmodified = get_string("lastmodified");
             echo "<div class=\"modified\">$strlastmodified: ".userdate($resource->timemodified)."</div>";
 
+            /*danhut added*/
+            print_container_end();
+    		echo '</td></tr></table>';
+    		/*end of danhut added*/
             print_footer($course);
+            
         }
 
-    }
+    //}
 
 }
 
