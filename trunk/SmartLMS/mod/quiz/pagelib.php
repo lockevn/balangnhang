d2c695bc9fcc03a0d1934735fb8843502d94d386
danhut <danhut@d5303d96-4916-11de-8855-c187e5f49513>
2009-08-06 16:04:30 +0000

    override 1 số method của page_base để enable các stick block vào quiz page và resource page

--- trunk/SmartLMS/mod/quiz/pagelib.php
+++ trunk/SmartLMS/mod/quiz/pagelib.php
@@ -41,6 +41,83 @@ class page_quiz extends page_generic_activity {
         }
         return $instance->position;
     }
+    
+    /*danhut added to add mod-quiz-view option in admin/stickyblocks.php*/
+	function user_is_editing() {
+        global $USER;
+		if (has_capability('moodle/my:manageblocks', get_context_instance(CONTEXT_SYSTEM)) && defined('ADMIN_STICKYBLOCKS')) {
+            return true;
+        }
+
+        return (!empty($USER->editing));
+    }
+    
+    
+	function url_get_parameters() {
+        if (defined('ADMIN_STICKYBLOCKS')) {
+            return array('pt' => ADMIN_STICKYBLOCKS);
+        } else {
+            return array();
+        }
+    }
+    
+	function url_get_path() {
+        global $CFG;
+        page_id_and_class($id,$class);
+        if ($id == PAGE_QUIZ_VIEW) {
+            return $CFG->wwwroot.'/mod/quiz/index.php';
+        } elseif (defined('ADMIN_STICKYBLOCKS')){
+            return $CFG->wwwroot.'/'.$CFG->admin.'/stickyblocks.php';
+        }
+    }
+    
+	function user_allowed_editing() {
+        page_id_and_class($id,$class);
+        if ($id == PAGE_QUIZ_VIEW) {
+            return true;
+        } else if (has_capability('moodle/my:manageblocks', get_context_instance(CONTEXT_SYSTEM)) && defined('ADMIN_STICKYBLOCKS')) {
+            return true;
+        }
+        return false;
+    }
+    
+	function print_header($title) {
+
+        global $USER, $CFG;
+
+        $replacements = array(
+                              '%fullname%' => get_string('stickyblocksquizview','admin')
+        );
+        foreach($replacements as $search => $replace) {
+            $title = str_replace($search, $replace, $title);
+        }
+
+        $site = get_site();
+
+        $button = update_mymoodle_icon($USER->id);
+        $nav = get_string('stickyblocksquizview','admin');
+        $header = $site->shortname.': '.$nav;
+        $navlinks = array(array('name' => $nav, 'link' => '', 'type' => 'misc'));
+        $navigation = build_navigation($navlinks);
+        
+        $loggedinas = user_login_string($site);
+
+        if (empty($CFG->langmenu)) {
+            $langmenu = '';
+        } else {
+            $currlang = current_language();
+            $langs = get_list_of_languages();
+            $langlabel = get_accesshide(get_string('language'));
+            $langmenu = popup_form($CFG->wwwroot .'/my/index.php?lang=', $langs, 'chooselang', $currlang, '', '', '', true, 'self', $langlabel);
+        }
+
+        print_header($title, $header,$navigation,'','',true, $button, $loggedinas.$langmenu);
+
+    }
+       
+    /*end of danhut added*/
 }
+
+page_map_class(PAGE_QUIZ_VIEW, 'page_quiz');
 
 ?>
