cf20a16512c583b89a9a77f73059d681a9a3e7da
danhut <danhut@d5303d96-4916-11de-8855-c187e5f49513>
2009-08-20 06:00:28 +0000

    new, delete personal glossary entry

--- trunk/SmartLMS/blocks/glossary/block_glossary.php
+++ trunk/SmartLMS/blocks/glossary/block_glossary.php
@@ -14,6 +14,8 @@ class block_glossary extends block_base {
 		}
 		$this->content = new stdClass();
 		$id = $this->config->cmid;
+		
+		/*check if module really exists*/
 		if (!empty($id)) {
 			if (! $cm = get_coursemodule_from_id('glossary', $id)) {
 				error("Course Module ID was incorrect");
@@ -33,70 +35,8 @@ class block_glossary extends block_base {
 		require_course_login($course->id, true, $cm);
 		$context = get_context_instance(CONTEXT_MODULE, $cm->id);
 		$glossaryID = $cm->instance;
-
-		$sqlStr = "SELECT concept as glossarypivot, definition, attachment 
-						FROM {$CFG->prefix}glossary_entries
-						WHERE glossaryid = $glossaryID
-						ORDER BY glossarypivot ASC";
-		$allentries = get_records_sql($sqlStr);
-		$textlib = textlib_get_instance();
-		if(empty($allentries))
-		{
-			$this->content->text .= get_string("noentry","glossary");
-			return $this->content;
-		}
-		$this->content->text .= '<div style="height:150px;overflow:auto">';
-		$this->content->text .= '<div class="glossaryexplain"></b>' . get_string("explainalphabet","glossary") . '<b></div>';
-		$upperpivotArr = array();
-		/*calculate the upperpivotArr to display as index bar */
-		foreach($allentries as $entry)
-		{
-			$pivot = $entry->glossarypivot;
-			$upperpivot = $textlib->strtoupper($pivot);
-			// Reduce pivot to 1cc if necessary
-			$upperpivot = $textlib->substr($upperpivot, 0, 1);
-			$upperpivotArr[] = $upperpivot;
-		}
-		/*display the index bar*/
-		$this->content->text .= $this->glossary_block_print_alphabet_links($upperpivotArr);
-
-		/*display the entry list*/
-		foreach($allentries as $entry)
-		{
-			$pivot = $entry->glossarypivot;
-			$upperpivot = $textlib->strtoupper($pivot);
-			// Reduce pivot to 1cc if necessary
-			$upperpivot = $textlib->substr($upperpivot, 0, 1);
-			
-			// if there's a group break
-			if ( $currentpivot != $upperpivot ) {
-
-				$currentpivot = $upperpivot;
-				$this->content->text .= '<div class="glossarycategoryheader">';
-				$pivottoshow = $currentpivot;
-					
-				$this->content->text .= "<h2><a name=\"prefix_$upperpivot\">".stripslashes_safe($pivottoshow)."</h2>";
-				$this->content->text .= "</div>\n";
-			}
-			$this->content->text .=
-            	"<table class=\"glossarypost dictionary\" cellspacing=\"0\">
-					<tbody>
-						<tr valign=\"top\">
-							<td class=\"entry\">
-								<div class=\"concept\">
-									<h3 class=\"nolink\">
-										<span class = \"nolink\">$entry->glossarypivot </span>
-									</h3>:
-									$entry->definition
-								</div>
-				
-							</td>
-						</tr>
-					</tbody>
-				</table>" ;
-
-		}
-		$this->content->text .= '</div>';			
+		/*generate content*/
+		$this->content->text = $this->print_entry_list($glossaryID);			
 		return $this->content;
 	}
 
@@ -121,6 +61,83 @@ class block_glossary extends block_base {
 			$this->config->text = 'Init config text';
 		}
 	}
+	
+	function print_entry_list($glossaryID) {
+	
+		GLOBAL $CFG;
+		$sqlStr = "SELECT concept as glossarypivot, definition, attachment 
+						FROM {$CFG->prefix}glossary_entries
+						WHERE glossaryid = $glossaryID
+						ORDER BY glossarypivot ASC";
+		$allentries = get_records_sql($sqlStr);
+		$textlib = textlib_get_instance();
+		$str = '';
+		if(empty($allentries))
+		{
+			$str .= get_string("noentry","glossary");
+			return $str;
+		}
+		
+		$upperpivotArr = array();
+		/*calculate the upperpivotArr to display as index bar */
+		foreach($allentries as $entry)
+		{
+			$pivot = $entry->glossarypivot;
+			$upperpivot = $textlib->strtoupper($pivot);
+			// Reduce pivot to 1cc if necessary
+			$upperpivot = $textlib->substr($upperpivot, 0, 1);
+			$upperpivotArr[] = $upperpivot;
+		}
+		$str .= '<div style="height:150px;overflow:auto">';
+		$str .= '<div class="glossaryexplain"></b>' . get_string("explainalphabet","glossary") . '<b></div>';
+		/*display the index bar*/
+		$str .= $this->glossary_block_print_alphabet_links($upperpivotArr);
+
+		/*display the entry list*/
+		foreach($allentries as $entry)
+		{
+			$pivot = $entry->glossarypivot;
+			$upperpivot = $textlib->strtoupper($pivot);
+			// Reduce pivot to 1cc if necessary
+			$upperpivot = $textlib->substr($upperpivot, 0, 1);
+			// if there's a group break
+			if ( $currentpivot != $upperpivot ) {
+				$str .= $this->print_group_beak($upperpivot, $currentpivot);
+			}
+			$str .= $this->print_entry($entry->glossarypivot, $entry->definition);
+		}
+		$str .= '</div>';			
+		return $str;
+	}
+	
+	function print_group_beak($upperpivot, $pivottoshow) {
+		
+		$str = '<div class="glossarycategoryheader">';					
+		$str .= "<h2><a name=\"prefix_$upperpivot\">".stripslashes_safe($pivottoshow)."</h2>";
+		$str .= "</div>\n";
+		return $str;
+	}
+	
+	function print_entry($term, $definition) {
+		$str = "<table class=\"glossarypost dictionary\" cellspacing=\"0\">
+					<tbody>
+						<tr valign=\"top\">
+							<td class=\"entry\">
+								<div class=\"concept\">
+									<h3 class=\"nolink\">
+										<span class = \"nolink\">$term </span>
+									</h3>:
+									$definition
+								</div>				
+							</td>
+						</tr>
+					</tbody>
+				</table>";
+		return $str;
+	}
+	
+	
+	
 
 	function glossary_block_print_alphabet_links($pivotArr) {
 		$retStr = "";
---
+++ trunk/SmartLMS/blocks/pers_glossary/glossary.js
@@ -0,0 +1,206 @@
+		var seldiv = document.getElementById('termSelector');
+		var statdiv = document.getElementById('status');
+		var letters = new Object();
+		var letter_arr = new Array();		
+		var lastselwhich = 1;		
+		var dh = ROOT_URL + "/blocks/pers_glossary/data.php";
+		
+		
+
+    	function renderStatus(o)
+			{
+				if (o.responseText !== undefined)
+				{
+					var ret = from_json(o.responseText);
+					if (ret.ok)
+					{
+						statdiv.innerHTML = "Success";
+						statdiv.style.backgroundColor = '#33ff33';
+						populateSelector();
+					}
+					else
+					{
+						statdiv.innerHTML = "Failed";
+						statdiv.style.backgroundColor = '#ff3333';
+					}
+				}
+			}
+
+    
+   
+	    	function renderSelector(o) {
+	    	
+				if (o.responseText !== undefined)
+				{
+					var ldhtml = '<div id="letterdiv" style="border-bottom: 1px solid #000"></div>';
+					varlisthtml = '<div id="termlist" style="height:200px;overflow:auto"></div>';
+					seldiv.innerHTML = ''; //o.responseText;
+					seldiv.innerHTML += ldhtml;
+					seldiv.innerHTML += varlisthtml;
+					var letterdiv = document.getElementById('letterdiv');
+					letters = new Object();
+					letter_arr = new Array();
+			
+					var ret = from_json(o.responseText);
+					if (ret.ok == 0)
+						return;
+					terms = ret.terms;
+					for (id in terms)
+					{
+						var t = terms[id];
+						var startc = t.sl_value.charAt(0).toUpperCase();
+						if (!letters[startc])
+						{
+							//console.log("hum..");
+							letters[startc] = new Array();
+							letter_arr.push(startc);
+						}
+						letters[startc].push(t);
+						//console.log(id, startc, t);
+						//console.log(letters);
+					}
+			
+					letter_arr = letter_arr.sort();
+					for (l in letter_arr)
+					{
+						letter = letter_arr[l];
+						letterdiv.innerHTML += '<a href="javascript:void(0)" onclick="showletter(\''+letter+'\')">'+letter+'|</a> ';
+					}
+					letterdiv.innerHTML += '<a href="javascript:void(0)" onclick="showAllLetter(letter_arr)">All</a> ';
+					// show the first letter stuff
+					//showletter(letter_arr[0]);
+					showAllLetter(letter_arr);
+				}
+			}
+    
+
+    	function showAllLetter(letter_arr)
+		{
+			var tmpStr = '<table width="100%">';
+			var tl = document.getElementById('termlist');
+			var startc;			
+			for(var j = 0; j < letter_arr.length; j++)
+			{
+				startc = letter_arr[j];
+				for (var i = 0; i < letters[startc].length; i++)
+				{
+					var term = letters[startc][i];
+					tmpStr += '<tr>';
+					tmpStr += '<td width="80%"><b>'+term.sl_value+'</b>:' + term.tl_value + '</td>';
+					tmpStr += '<td width="10%"><a href="javascript:void(0)" onclick="deleteterm(\''+ startc +'\','+i+')"><img alt="Delete" src="' + PIXPATH + '/t/delete.gif"/></a></td>';
+					tmpStr += '<td width="10%"><a href="javascript:void(0)" onclick="editterm(\''+ startc +'\','+i+')"><img alt="Edit" src="' + PIXPATH + '/t/edit.gif"/></a></td>';
+					tmpStr += '</tr>';									
+				}
+			}
+			tmpStr += '</table>';
+			tl.innerHTML = tmpStr;
+		}
+ 
+    
+ 
+    	function showletter(startc)
+		{
+			var tl = document.getElementById('termlist');
+			var tmpStr = '<table width="100%"><tbody>';
+			for (var i = 0; i < letters[startc].length; i++)
+			{
+				var term = letters[startc][i];
+				tmpStr += '<tr>';
+					tmpStr += '<td width="80%"><b>'+term.sl_value+'</b>:' + term.tl_value + '</td>';
+					tmpStr += '<td width="10%"><a href="javascript:void(0)" onclick="deleteterm(\''+ startc +'\','+i+')"><img alt="Delete" src="' + PIXPATH + '/t/delete.gif"/></a></td>';
+					tmpStr += '<td width="10%"><a href="javascript:void(0)" onclick="editterm(\''+ startc +'\','+i+')"><img alt="Edit" src="' + PIXPATH + '/t/edit.gif"/></a></td>';
+					tmpStr += '</tr>';					
+			}
+			tmpStr += '</tbody></table>';
+			tl.innerHTML = tmpStr;
+		}
+
+    
+
+    	function populateSelector()
+		{
+			var qs;
+			var ws;		
+		
+			qs = '?data='+encodeURIComponent(to_json({act: 'get'}));			
+			var callback = {
+				success: renderSelector,
+				failure: handleFailure,
+			};
+			var request = YAHOO.util.Connect.asyncRequest('GET', dh+qs, callback); 
+		}
+    
+    
+    
+    	function saveForm()
+		{
+			// get the form
+			var f = document.getElementById('termform');
+			var act = 'insert';
+			if (f.id.value != -1)
+				act = 'edit';
+		
+			var qs = '?data='+encodeURIComponent(to_json({	act: act,							
+									sl_value: f.sl_value.value,
+									tl_value: f.tl_value.value,							
+									id: f.id.value}));
+			var callback = {
+				success: renderStatus,
+				failure: handleFailure,
+			};
+			var request = YAHOO.util.Connect.asyncRequest('GET', dh+qs, callback); 
+		
+    	}
+    
+
+		function editterm(startc,i)
+		{
+			var term = letters[startc][i];
+			var f = document.getElementById('termform');
+			// add hidden id field for editing
+			f.id.value = term.id;
+			// populate form
+			f.sl_value.value = term.sl_value;
+			f.tl_value.value = term.tl_value;
+		//	f.sl_notes.value = term.sl_notes;
+		//	f.tl_notes.value = term.tl_notes;
+		}
+    
+
+		function deleteterm(startc,i)
+		{
+			var term = letters[startc][i];
+			// get the form
+			var f = document.getElementById('termform');
+			//f.id.value = term.id;
+			var act = 'delete';
+			var qs = '?data='+encodeURIComponent(to_json({	act: act,																								
+									id: term.id}));
+			var callback = {
+				success: renderStatus,
+				failure: handleFailure,
+			};
+			var request = YAHOO.util.Connect.asyncRequest('GET', dh+qs, callback);
+			
+		}
+    
+ 	
+    	function handleFailure(o)
+		{
+			/*alert("xmlhttprequest failed!\nTransaction id: "+o.tId+"\nHTTP status: "+o.status+"\nStatus text: "+o.statusText);*/
+			 alert("xmlhttprequest failed!");
+		}
+		
+		function newEntry()
+		{
+			var f = document.getElementById('termform');			
+			f.id.value = -1;
+			f.sl_value.value = '';
+			f.tl_value.value = '';
+			
+		}	
+		
+		populateSelector();
+		
+		
+		
