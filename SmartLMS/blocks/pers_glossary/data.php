e52df1365ebd8780e8cee83b11854f3b43839150
danhut <danhut@d5303d96-4916-11de-8855-c187e5f49513>
2009-08-20 06:00:28 +0000

    new, delete personal glossary entry

--- SmartLMS/blocks/pers_glossary/data.php
+++ SmartLMS/blocks/pers_glossary/data.php
@@ -22,14 +22,14 @@ switch($d->act)
 		{
 			foreach ($data as $term)
 			{
-				if ($d->source_lang && $term->source_lang == $d->source_lang)
-				{
+//				if ($d->source_lang && $term->source_lang == $d->source_lang)
+//				{
+//					array_push($ret["terms"], $term);
+//				}
+//				if ($d->target_lang && $term->target_lang == $d->target_lang)
+//				{
 					array_push($ret["terms"], $term);
-				}
-				if ($d->target_lang && $term->target_lang == $d->target_lang)
-				{
-					array_push($ret["terms"], $term);
-				}
+//				}
 			}
 			$ret["ok"] = 1;
 			print json_encode($ret);
@@ -66,6 +66,14 @@ switch($d->act)
 		$term->tl_notes = $d->tl_notes;
 
 		$r = update_record('pgq_terms', $term);
+		// XXX: did this fail?
+		if ($r)
+			print json_encode(array("ok" => 1));
+		else
+			print json_encode(array("ok" => 0));
+		break;
+	case 'delete':					
+		$r = delete_records('pgq_terms', 'id', $d->id, 'userid', $USER->id);
 		// XXX: did this fail?
 		if ($r)
 			print json_encode(array("ok" => 1));
--- SmartLMS/blocks/pers_glossary/db/mysql.sql
+++ SmartLMS/blocks/pers_glossary/db/mysql.sql
@@ -1,20 +1,21 @@
-CREATE TABLE prefix_pgq_langs
+/*CREATE TABLE prefix_pgq_langs
 (
 	id bigint(10) not null primary key auto_increment,
 	userid bigint(10) not null references prefix_user,
 	name varchar(64) not null,
 	unique(userid,name)
 );
+*/
 
 CREATE TABLE prefix_pgq_terms
 (
 	id bigint(10) not null primary key auto_increment,
-	userid bigint(10) not null references prefix_user,
-	source_lang bigint(10) not null references prefix_pgq_langs,
-	target_lang bigint(10) not null references prefix_pgq_langs,
+	userid bigint(10) not null,
+/*	source_lang bigint(10) not null references prefix_pgq_langs,
+	target_lang bigint(10) not null references prefix_pgq_langs, */
 	sl_value varchar(64) not null,
 	tl_value varchar(64) not null,
 	sl_notes text not null,
 	tl_notes text not null,
-	unique (source_lang,target_lang,sl_value)
+	unique (sl_value)
 );
