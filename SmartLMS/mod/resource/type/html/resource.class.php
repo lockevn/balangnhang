29d682e230dce261b065cf0c91031a2729f461c9
danhut <danhut@d5303d96-4916-11de-8855-c187e5f49513>
2009-08-20 13:31:17 +0000

    danhut sửa width mặc định cho right blocks trong html resource page

--- SmartLMS/mod/resource/type/html/resource.class.php
+++ SmartLMS/mod/resource/type/html/resource.class.php
@@ -66,8 +66,10 @@ function display() {
 
     /*danhut addded to enable blocks in resource page*/
     $PAGE       = page_create_instance($resource->id);
-    $pageblocks = blocks_setup($PAGE, BLOCKS_PINNED_BOTH);
-    $blocks_preferred_width = bounded_number(180, blocks_preferred_width($pageblocks[BLOCK_POS_LEFT]), 210);
+    $pageblocks = blocks_setup($PAGE, BLOCKS_PINNED_BOTH);   
+    $left_blocks_preferred_width = 180;
+    $right_blocks_preferred_width = 250;
+    //$blocks_preferred_width = bounded_number(180, blocks_preferred_width($pageblocks[BLOCK_POS_LEFT]), 210);
     /*end of danhut addded*/
     
    /*danhut commented out: khôgn hiển thị toàn bộ block ở course page vào module page nữa*/
@@ -138,7 +140,7 @@ function display() {
             /*danhut added*/
 		    echo '<table id="layout-table"><tr>';
 			if(!empty($CFG->showblocksonmodpages) && (blocks_have_content($pageblocks, BLOCK_POS_LEFT) || $PAGE->user_is_editing())) {
-		        echo '<td style="width: '.$blocks_preferred_width.'px;" id="left-column">';
+		        echo '<td style="width: '.$left_blocks_preferred_width.'px;" id="left-column">';
 		        print_container_start();
 		        blocks_print_group($PAGE, $pageblocks, BLOCK_POS_LEFT);
 		        print_container_end();
@@ -157,7 +159,7 @@ function display() {
     		echo '</td>';
     		
         	if(!empty($CFG->showblocksonmodpages) && (blocks_have_content($pageblocks, BLOCK_POS_RIGHT) || $PAGE->user_is_editing())) {
-		        echo '<td style="width: '.$blocks_preferred_width.'px;" id="right-column">';
+		        echo '<td style="width: '.$right_blocks_preferred_width.'px;" id="right-column">';
 		        print_container_start();
 		        blocks_print_group($PAGE, $pageblocks, BLOCK_POS_RIGHT);
 		        print_container_end();
