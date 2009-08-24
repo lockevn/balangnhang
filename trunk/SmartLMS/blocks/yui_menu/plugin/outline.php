1e2d06a15b119f3b112b85cf670b4ea2f9928dff
danhut <danhut@d5303d96-4916-11de-8855-c187e5f49513>
2009-08-24 15:06:01 +0000

    content tree trong giao dien hoc tap

---
+++ trunk/SmartLMS/blocks/yui_menu/plugin/outline.php
@@ -0,0 +1,311 @@
+<?php
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+/**
+ * Builds a nested outline from course contents
+ */
+
+class yui_menu_plugin_outline extends yui_menu_plugin {
+    
+    function add_items($list, $block) {
+        global $CFG, $COURSE;
+        if (in_array($COURSE->format, array('weeks','weekscss'))) {
+            $viewall = 'week=0';
+        } else {
+            $viewall = 'topic=all';
+        }
+        $outline = new yui_menu_item_link($this,
+            get_string('outline', 'block_yui_menu'),
+            "{$CFG->wwwroot}/course/view.php?id={$COURSE->id}&$viewall",
+            "{$CFG->wwwroot}/blocks/yui_menu/icons/viewall.gif");
+        $outline->children = $this->sections($block->config);
+        $list[$this->id] = $outline;
+    }
+    
+    function sections($config) {
+        global $COURSE, $CFG, $USER, $THEME, $PAGE;
+ 		
+        // probably inefficient, but it works
+        get_all_mods($COURSE->id, $mods, $modnames, $modnamesplural, $modnamesused);
+        
+//        /*danhut added, only display structure of current lesson*/
+//        
+//        $id = optional_param("id");
+//        if(!empty($id)) {
+//        	$cm = get_coursemodule_from_id($PAGE->activityname, $id);
+//        	$sections = get_records("course_sections", "id", $cm->section, "",
+//                       "section, id, course, summary, sequence, visible");
+//        }
+//        else if (empty($sections)) {                      
+//        /*end of danhut added*/        
+        // sections
+        $sections = get_all_sections($COURSE->id);
+        
+        //}
+        
+        // name for sections
+        $sectionname = get_string("name{$COURSE->format}","format_{$COURSE->format}");
+        // TODO: this fallback should be unnecessary
+        if ($sectionname == "[[name{$COURSE->format}]]") {
+            $sectionname = get_string("name{$COURSE->format}");
+        }
+        $return = array();
+        // check what the course format is like
+        // highlight for current week or highlighted topic
+        if (in_array($COURSE->format, array('weeks','weekscss'))) {
+            $format = 'week';
+            $highlight = ceil((time()-$COURSE->startdate)/604800);
+        } else {
+            $format = 'topic';
+            $highlight = $COURSE->marker;
+        }
+        $modinfo = unserialize($COURSE->modinfo);
+
+        // I think $display is the section currently being displayed
+        // Why are we calling course_set_display?
+        // For Moodle 2.0 we should just use $PAGE and check type
+        // and also $PAGE->activityrecord
+        $path = str_replace($CFG->httpswwwroot.'/', '', $CFG->pagepath);
+        if (substr($path, 0, 7) == 'course/') {
+            //TODO: this code is hackish, we shouldn't use course_set_display
+            # get current section being displayed
+            $week = optional_param('week', -1, PARAM_INT);
+            if ($week != -1) {
+                // the course format should already be doing this
+                $display = course_set_display($COURSE->id, $week);
+            } else {
+                if (isset($USER->display[$COURSE->id])) {
+                    $display = $USER->display[$COURSE->id];
+                } else {
+                    $display = course_set_display($COURSE->id, 0);
+                }
+            }
+        } elseif (substr($path, 0, 4) == 'mod/') {
+            // Moodle 2: use $PAGE->activityrecord->section;
+            $id = optional_param('id', -1, PARAM_INT);
+            if ($id == -1) $display = 0;
+            else {
+                $sql="select section from {$CFG->prefix}course_sections where id=(select section from {$CFG->prefix}course_modules where id=$id)";
+                $row=get_record_sql($sql);
+                $display=$row->section;
+            }
+        } else {
+            $display = 0;
+        }
+
+        foreach ($sections as $section) {
+            // don't show the flowing sections
+            if (!($section->visible && // invisible ones
+                  $section->section && // the one at the very top
+                  // anything above the courses section limit
+                  $section->section <= $COURSE->numsections
+                  )) { continue;}
+
+            $sectionText = ucwords($sectionname)." ".$section->section . ": " .trim($section->summary);
+
+//            if (empty($sectionText)) {
+//                $sectionText = ucwords($sectionname)." ".$section->section;
+//            } else {
+                $sectionText = $this->truncate_html(filter_text($sectionText,$COURSE->id),
+                    $config);
+            //}
+            // expand section if it's the one currently displayed
+            $expand = false;
+            if ($section->section == $display) {
+                $expand = true;
+            }
+            $sectionstyle = 'yui_menu_icon_section';
+            // highlight marked section
+            if ($section->section == $highlight) {
+                $sectionstyle .= ' highlight';
+            }
+            $iconpath = $CFG->wwwroot;
+            if ($THEME->custompix) $iconpath .= "/theme/{$CFG->theme}";
+            $iconpath .= '/pix';
+            // decide what URL we want to use
+            // A lot of this should really be done by the course format
+            //
+            // = intoaction config values =
+            // * 'introhide' link to the section page (this effectively
+            //   hides the other sections
+            // * 'introscroll' link to the fragment id of the section on
+            //   on the current page
+            
+            // whether or not any of the sections are hidden
+//            $hidden=false;
+//            foreach(array('topic','week') as $param) {
+//            	if (isset($_GET[$param]) && $_GET[$param]!='all'){
+//            		$hidden = true;
+//            	}
+//            }
+//            $introaction = (isset($config->introaction) ?
+//                    $config->introaction : 'introhide');
+//            if ($introaction == 'introhide' || $hidden) {
+//                // link to the section, this will effectively hide all
+//                // the other sections
+//                $sectionUrl =  "{$CFG->wwwroot}/course/view.php?id={$COURSE->id}"
+//                        . "&$format={$section->section}";
+//            } else {
+//                // this pretty much just a hack
+//                // use $PAGE in Moodle 2 for great justice
+//                if (strpos($_SERVER['REQUEST_URI'],'course/view.php')!=0) {
+//                    $sectionUrl = "#section-{$section->section}";
+//                } else {
+//                    $sectionUrl = false;
+//                }
+//            }
+
+//            if ($sectionUrl === false) {
+//                $item = new yui_menu_item($this, $sectionText, $iconpath . '/i/one.gif');
+//            } else {
+//                $item = new yui_menu_item_link($this, $sectionText, $sectionUrl, $iconpath . '/i/one.gif');
+//            }
+
+            
+            if (isset($section->sequence)) {
+                $sectionmods = explode(",", $section->sequence);
+            } else {
+                $sectionmods = array();
+            }
+            
+            $sectionUrl = $this->getLessonStartUrl($mods, $sectionmods);
+            /*nếu section có chứa 1 StartLesson activity thì sẽ dùng link đó cho section node trên cây*/ 
+            if( $sectionUrl === false) {
+            	$item = new yui_menu_item($this, $sectionText, $iconpath . '/i/one.gif');
+            } 
+            else {
+            	$item = new yui_menu_item_link($this, $sectionText, $sectionUrl, $iconpath . '/i/one.gif');
+            }            
+            $item->expand = $expand;
+
+            /*to store most recent parent node*/
+            $recentParentNode = $item;
+            foreach ($sectionmods as $modnumber) {
+                if (empty($mods[$modnumber])) continue;
+                $mod = $mods[$modnumber];                
+                // danhut modified: only show label, resource, quiz, assignment, survey, wiki
+                if (!$mod->visible ||  
+                	($mod->modname != 'label'
+                	&& $mod->modname != 'resource'
+                	&& $mod->modname != 'assignment'
+                	&& $mod->modname != 'survey'
+                	&& $mod->modname != 'wiki'
+                	&& $mod->modname != 'quiz')) {
+                		continue;
+                	}
+                // figure out the text and url
+                $text = urldecode($modinfo[$modnumber]->name);
+                if (!empty($CFG->filterall)) {
+                    $text = filter_text($text, $COURSE->id);
+                }                
+                if (trim($text) == '')  $text = $mod->modfullname;
+                $text = $this->truncate_html($text, $config);
+                
+                /*trim chracter + */
+                if($mod->modname == 'label') {
+                	$text = trim($text, '+');
+                }
+                $url = "{$CFG->wwwroot}/mod/{$mod->modname}/view.php?id={$mod->id}";
+                $name = "yui_menu_mod_{$mod->modname}_$modnumber";
+                // figure out if it is the current page
+                $pageurl = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+                $pageurl = '~https?://'.preg_quote($pageurl,'~').'~';
+                if (preg_match($pageurl, $CFG->wwwroot . $url)) {
+                    $style = "yui_menu_mod_{$mod->modname} highlight";
+                } else {
+                    $style = "yui_menu_mod_{$mod->modname}";
+                }
+                $icon = "$iconpath/mod/{$mod->modname}/icon.gif";
+                if ($mod->modname == 'resource') {
+                    $info = resource_get_coursemodule_info($mod);
+                    if (isset($info) && isset($info->icon)) {
+                        $icon = "$CFG->pixpath/$info->icon";
+                    }
+                }
+                
+                /*danhut added: nếu indent = 0, add to Lesson node, nếu indent = 1, add to node có indent = 0 ngay trước đó*/
+                
+                $child = new yui_menu_item_link($this, $text, $url, $icon);
+                $child->style = $style;
+                
+                if($mod->indent == 0) {
+                	$item->children[$modnumber] = $child;	
+                	$recentParentNode = $child;
+                } 
+                else if($mod->indent == 1) {
+                	$recentParentNode->children[$modnumber] = $child;
+                }
+                
+            }
+            $return[] = $item;
+        }
+        return $return;
+    }
+    /**
+     * Filters html and junk, truncates result
+     *
+     * @param $html: string to filter
+     * @param $max_size: length of largest piece when done
+     * @param $trunc: string to append to truncated pieces
+     */
+    function truncate_html($html, $config) {
+        $max_size = (isset($config->maxsize) ? $config->maxsize : '19');
+        $trunc = (isset($config->trunc) ? $config->trunc : '...');
+        $text = preg_replace_callback('|</?([^\s>]*).*?>|smi',
+            array($this, 'filter_tag'), $html);
+        // decode html entities, they will be encoded later with
+        // htmlspecialchars
+        $text= html_entity_decode($text, ENT_QUOTES, 'UTF-8');
+        // trim ends of whitespace
+        $text = trim($text);
+        // filter any extra whitespace
+        $text = preg_replace('|\s\s+|m', ' ', $text);
+        // trunkcate string size
+        $tl = textlib_get_instance();
+        if ($tl->strlen($text) > $max_size) {
+            $text = $tl->substr($text, 0, ($max_size - $tl->strlen($trunc))).$trunc;
+        }
+        return $text;
+    }
+    
+    /**
+     * Strip html tags
+     *
+     * replaces it with a single space or and emtpy string depending
+     * one wheth it looks like a block or inline level element.
+     */
+    function filter_tag($matches) {
+        $inline = array('a','abbr','acronym','b','big','cite','code',
+            'del','dfn','em','i','ins','kbd','q','strong','s','samp',
+            'small','span','sup','sub','tt','u','var');
+        $tag = strtolower($matches[1]);
+        // inline elements are displayed inline
+        if (in_array($tag, $inline)) return '';
+        // block level elements are given a single space separation
+        return ' ';
+    }
+    
+    function getLessonStartUrl($mods, $sectionmods) {
+    	$sectionUrl = false;
+    	Global $CFG;
+    	foreach ($sectionmods as $modnumber) {
+              	if (empty($mods[$modnumber])) continue;
+                $mod = $mods[$modnumber];                
+                if($mod->modname === 'start') {
+                	$sectionUrl = "{$CFG->wwwroot}/mod/start/view.php?id=$mod->id";
+                	break;                	                
+                }                
+    	}
+    	return $sectionUrl;
+    }
+
+}
