c38bfaaeced16e8e4e5fed94b71a92ed89c06089
danhut <danhut@d5303d96-4916-11de-8855-c187e5f49513>
2009-09-09 08:10:20 +0000

    recorder

---
+++ SmartLMS/mod/nanogong/version.php
@@ -0,0 +1,6 @@
+<?php // $Id: version.php,v 3.1 2008/08/20 00:00:00 gibson Exp $
+
+$module->version  = 2008082000;  // The current module version (Date: YYYYMMDDXX)
+$module->cron     = 0;           // Period for cron to check this module (secs)
+
+?>
