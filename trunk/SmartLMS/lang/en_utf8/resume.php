60a00ec9d05ce3013e05d7f17cc40d54f9fd22e9
danhut <danhut@d5303d96-4916-11de-8855-c187e5f49513>
2009-09-14 08:02:09 +0000

    resume course

---
+++ trunk/SmartLMS/lang/en_utf8/resume.php
@@ -0,0 +1,8 @@
+
+<?PHP // $Id: label.php,v 1.2 2003/11/18 07:28:46 moodler Exp $ 
+      // label.php - created with Moodle 1.2 development (2003111400)
+
+$string['resume_course'] = 'Resume course';
+
+
+?>
