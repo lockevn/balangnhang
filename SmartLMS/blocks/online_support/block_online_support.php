967054ddd7ed6a7a03ab6fac3923ff408d6a66da
nttuyen87 <nttuyen87@d5303d96-4916-11de-8855-c187e5f49513>
2009-12-18 10:33:50 +0000

    commit block_online_support.php - remove commented code!

--- SmartLMS/blocks/online_support/block_online_support.php
+++ SmartLMS/blocks/online_support/block_online_support.php
@@ -29,38 +29,6 @@ class block_online_support extends block_base {
         $this->content = new stdClass();
         $options = new object();
         $options->noclean = true;    // Don't clean Javascripts etc
-        $str = "";  
-        /*$str .= '<script type="text/javascript">';
-		$str .= '	function onBrowse(url) {';
-		$str .= '    var lx = (screen.width - 470) / 2;';
-		$str .= '    var tx = (screen.height - 400) / 2;';
-		
-		$str .= '    var settings = "toolbar=no,";';
-		$str .= '    settings += " location=no,";';
-		$str .= '    settings += " directories=no,";';
-		$str .= '    settings += " status=no,";';
-		$str .= '    settings += " menubar=no,";';
-		$str .= '    settings += " scrollbars=no,";';
-		$str .= '    settings += " resizable=no,";';
-		$str .= '    settings += " width=470,";';
-		$str .= '    settings += " height=400,";';
-		
-		$str .= '    var newwin = window.open(url,"",""+ settings +" left="+ lx +", top="+ tx +"");';
-		$str .= '    return false;';
-		$str .= '}';
-		$str .= '</script>';*/
-		 
-        //$str .= '<div style="display: block; width:200px; margin-left:10px; padding-left: 10px;">';
-        
-        //$str .= '<p>Demo online</p>';
-        //$str .= '<p><a href="#" onclick = "window.open(\'http://localhost/message/discussion.php?id=22\',\'Chat\', \'toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=no,width=470,height=400\'); return false;" target="_blank">Chat with teacher</a></p>';
-        
-        //$teachers = get_list_teacher();
-        //foreach($teachers as $teacher) {
-        //	$str .= '<a href="http://localhost/message/discussion.php?id='.$teacher['id'].'" target="_blank">'.$teacher['name'].'</a><br/>';
-        //}
-        
-        //$str .= '</div>';
         
         $str = "";
         $str .= '<div style="display: block; width:200px; padding-left: 10px;">';
