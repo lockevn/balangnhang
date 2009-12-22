4ca1b6baeaf0de527bf0e8d9220492e7a16c4088
nttuyen87 <nttuyen87@d5303d96-4916-11de-8855-c187e5f49513>
2009-12-22 08:06:27 +0000

    lang of block_online_support

--- trunk/SmartLMS/lang/vi_utf8/block_online_support.php
+++ trunk/SmartLMS/lang/vi_utf8/block_online_support.php
@@ -1,4 +1,4 @@
 <?php
 $string['block_online_support_title']='Online support';
-$string['block_online_support_headline']='support nick';
-$string['block_online_support_no_teacher_online']='There is noteacher online now';
+$string['block_online_support_headline']='Giáo viên';
+$string['block_online_support_no_teacher_online']='Hiện tại không có giáo viên nào online';
