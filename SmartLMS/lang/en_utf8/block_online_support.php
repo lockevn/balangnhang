b1b2b531f23e135e6f665592c85d433eb0fc6760
nttuyen87 <nttuyen87@d5303d96-4916-11de-8855-c187e5f49513>
2009-12-22 05:09:40 +0000

    online_support

--- SmartLMS/lang/en_utf8/block_online_support.php
+++ SmartLMS/lang/en_utf8/block_online_support.php
@@ -1,3 +1,4 @@
 <?php
 $string['block_online_support_title']='Online support';
 $string['block_online_support_headline']='Teachers List';
+$string['block_online_support_no_teacher_online']='There is noteacher online now';
